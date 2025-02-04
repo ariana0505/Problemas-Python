2a5e913c33668042939cc121a4e370dbbbb69580
ariana0505 <penaariana075@gmail.com>
2025-02-04 15:08:19 -0500

    dia2

--- datos-simples/ej5.py
+++ datos-simples/ej5.py
@@ -1,1 +1,4 @@
-horas = float(input())
+horas = float(input("Ingrese las horas trabajadas: "))
+Precio_hora = float(input("Ingrese el coste por hora: "))
+
+print(f"El pago que le corresponde es: {horas * Precio_hora}")

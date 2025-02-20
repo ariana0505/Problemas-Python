0ae952233c253a7abccbfc14735668a92acb9ca6
ariana0505 <penaariana075@gmail.com>
2025-02-19 23:13:41 -0500

    dia 6

--- bucles/ej9.py
+++ bucles/ej9.py
@@ -1,20 +1,23 @@
 #mi forma
 
-verificacion = False
-contraseña = "Hola123.321"
-while verificacion == False:
-    intentoIngreso = input("Ingrese su contraseña: ")
-    if(intentoIngreso  == contraseña):
-        print("¡Bienvenido!")
-        verificacion = True
 
-    else:
-        print("error en la contraseña, intente denuevo")
+# contraseña = "Hola123.321"
+# while True:
+#     intentoIngreso = input("Ingrese su contraseña: ")
+#     if(intentoIngreso  == contraseña):
+#         print("¡Bienvenido!")
+#         break
+#     else:
+#         print("error en la contraseña, intente denuevo")
 
 #Su forma:
 
 key = "contraseña"
 password =""
+i = 0
 while password != key:
+    if i != 0:
+        print("ERROR")
     password = input("Introduce la contraseña: ")
+    i = 1
 print("Contraseña correcta")

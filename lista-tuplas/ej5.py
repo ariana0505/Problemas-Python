e65254cc074aa33de2a2ac7f634cf83f2a6dc33e
ariana0505 <penaariana075@gmail.com>
2025-02-21 16:46:41 -0500

    dia 8

--- lista-tuplas/ej5.py
+++ lista-tuplas/ej5.py
@@ -1,1 +1,7 @@
 #Escribir un programa que almacene en una lista los números del 1 al 10 y los muestre por pantalla en orden inverso separados por comas.
+
+numeros = []
+for i in range(1,11):
+    numeros.append(i)
+for i in numeros[::-1]:
+    print(i,end=",")
--- lista-tuplas/ej6.py
+++ lista-tuplas/ej6.py
@@ -1,1 +1,15 @@
 #Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista, pregunte al usuario la nota que ha sacado en cada asignatura y elimine de la lista las asignaturas aprobadas. Al final el programa debe mostrar por pantalla las asignaturas que el usuario tiene que repetir.
+asignaturas = ["Matemáticas", "Física", "Química", "Historia", "Lengua"] 
+notas = {}
+cursosJalados = []
+for asignatura in asignaturas:
+    nota = float(input(f"ingrese su nota de {asignatura}: "))
+    notas[asignatura] = nota
+
+
+for curso, nota in notas.items():
+    if (nota <= 10):
+        cursosJalados.append(curso)
+        
+print("Los cursos que tienes que repetir son: ")
+print(*cursosJalados, sep=", " )

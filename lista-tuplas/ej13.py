2de0eac7faa7ab8af906957040a79fee47a10a30
ariana0505 <penaariana075@gmail.com>
2025-02-25 23:05:38 -0500

    avanze

--- lista-tuplas/ej13.py
+++ lista-tuplas/ej13.py
@@ -1,5 +1,10 @@
 #Escribir un programa que pregunte por una muestra de números, separados por comas, los guarde en una lista y muestre por pantalla su media y desviación típica.
+import math
+
 muestra_num = input("Ingrese una muestra de números, separados por comas: ")
 lista = list(map(lambda i: int(i),muestra_num.split(",")))
-resultado = sum(lista)/len(lista)
-print(f"Tu media es: {resultado} :D")
+media = sum(lista)/len(lista)
+print(f"Tu media es: {media} :D")
+sumatoria = sum((x - media) ** 2 for x in lista)
+desviacion = math.sqrt(sumatoria / (len(lista) - 1))
+print(desviacion)

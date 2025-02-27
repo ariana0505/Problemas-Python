317b8441ddfbc4f4ec6897a0285a105f7a29c1d8
ariana0505 <penaariana075@gmail.com>
2025-02-27 14:42:17 -0500

    nuevo

--- lista-tuplas/ej11.py
+++ lista-tuplas/ej11.py
@@ -1,1 +1,9 @@
 #Escribir un programa que almacene los vectores (1,2,3) y (-1,0,2) en dos listas y muestre por pantalla su producto escalar.
+import numpy as np
+
+A = np.array([1, 2, 3])
+B = np.array([-1, 0, 2])
+
+producto_escalar = np.dot(A, B)
+
+print(producto_escalar)  
--- lista-tuplas/ej12.py
+++ lista-tuplas/ej12.py
@@ -1,1 +1,13 @@
 #Escribir un programa que almacene las matrices a y b en una lista y muestre por pantalla su producto.Nota: Para representar matrices mediante listas usar listas anidadas, representando cada vector fila en una lista.
+import numpy as np
+
+A = np.array([[1, 2, 3],
+              [4, 5, 6]])
+
+B = np.array([[7, 8],
+              [9, 10],
+              [11, 12]])
+
+C = np.dot(A, B)
+
+print(C)

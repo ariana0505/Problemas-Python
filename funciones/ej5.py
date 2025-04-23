cde665fe6ce787c23d9f2369f675f86c067796c0
ariana0505 <penaariana075@gmail.com>
2025-04-23 17:25:34 -0500

    solution to a problem related to the volume of a cylinder

---
+++ funciones/ej5.py
@@ -0,0 +1,11 @@
+def _area_of_circle(radius:float):
+    area = radius**2
+    return f"{area}π"
+
+def _volume_of_cylinder(radius:float, height: float):
+    area_cicle = _area_of_circle(radius)
+    area_cicle = area_cicle.split("π")[0]
+    volume = area_cicle * height
+    return f"Volume is {volume}"
+print(f"{_area_of_circle(3)}")
+

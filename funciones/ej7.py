0870b5976dca9a33f6848e01f95c3c20fd66f69e
ariana0505 <penaariana075@gmail.com>
2025-05-16 01:19:10 -0500

    solution to a problem about a list cubed

---
+++ funciones/ej7.py
@@ -0,0 +1,10 @@
+def numbers_cubed(numbers):
+    new_list_of_numbers = []
+    for i in numbers:
+        new_list_of_numbers.append(i**3)
+    return new_list_of_numbers 
+
+# def numbers_cubed(numbers):
+#     return [i ** 3 for i in numbers]
+
+#[nueva_expresión for elemento in iterable]
